41dfca77eb61f1532e22386489ba42e65810554d
Jianhao Yu <yujianhao1314@gmail.com>
2023-03-07 03:02:47 +0000

    Created rails basic CRUD features

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # see all tasks
+  get "tasks", to: "tasks#index"
+
+  # create a new task
+  get "tasks/new", to: "tasks#new"
+
+  # post the new task
+  post "tasks", to: "tasks#create"
+
+  # check the details of a task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # edit a task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  # update a task
+  patch "tasks/:id", to: "tasks#update"
+
+  # delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
